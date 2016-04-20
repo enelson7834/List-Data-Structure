570892cacac598d213c26ebdbfaf435679898d4c
Eugene Nelson <enelson@ecc-d-11vm.rd.unr.edu>
2016-04-20 09:49:19 -0700

    List Node Change

--- listnode.cpp
+++ listnode.cpp
@@ -25,16 +25,100 @@ ListNode::~ListNode( )
 
 bool ListNode::insertAfter( int a )
 {
+	Node* temp = new Node( a, NULL );
+
+	if( this->isEmpty( ) )
+	{
+		this->head = this->cursor = temp;
+
+		return true;
+	}
+	else
+	{
+		temp->next = this->cursor->next;
+
+		this->cursor->next = temp;
+		this->cursor = temp;
+
+		return true;
+	}
+}
+
+bool ListNode::insertBefore( int a )
+{
+	Node* temp = new Node( a, this->cursor );
+
+	if( this->isEmpty( ) )
+	{
+		this->head = this->cursor = temp;
+		return true;
+	}
+	else
+	{
+		temp = this->cursor->data;
+
+		this->cursor->data = a;
+		if( this->cursor == this->head )
+		{
+			this->head = temp;
+		}
+
+		this->cursor = temp;
+
+		return true;
+	}
 
 }
 
-bool insertBefore(int);
-bool get(int&) const;
-bool remove(int&);
+bool ListNode::get( int& a ) const
+{
+	if( this->isEmpty( ) )
+	{
+		return false;
+	}
+	else
+	{
+		a = this->cursor->data;
+		return true;
+	}
+}
 
-void clear();
+bool ListNode::remove( int& a )
+{
+	if( this->isEmpty( ) )
+	{
+		return false;
+	}
+	else
+	{
+		a = this->cursor->data;
 
-bool goToBeginning( )
+		Node* temp1 = this->cursor->next;
+		Node* temp2 = this->cursor;
+		this->goToPrior( );
+
+		delete temp2;
+		temp2 = NULL;
+
+		this->cursor->next = temp1;
+		temp1 = NULL;
+
+		if( this->cursor->next == NULL )
+		{
+			this->cursor = this->head;
+		}
+		else
+		{
+			this->cursor = this->cursor->next;
+		}
+
+		return true;
+	}
+}
+
+void ListNode::clear();
+
+bool ListNode::goToBeginning( )
 {
 	if( this->isEmpty( ) )
 	{
@@ -46,13 +130,72 @@ bool goToBeginning( )
 	}
 }
 
-bool goToEnd();
+bool ListNode::goToEnd( )
+{
+	if( this->isEmpty( ) )
+	{
+		return false;
+	}
+	else
+	{
+		while( this->cursor->next != NULL )
+			this->cursor = this->cursor->next;
+		return true;
+	}
+}
 
-bool goToNext();
-bool goToPrior();
+bool ListNode::goToNext( )
+{
+	if( this->isEmpty( ) )
+	{
+		return false;
+	}
+	else if( this->cursor->next == NULL )
+	{
+		return false;
+	}
+	else
+	{
+		this->cursor = this->cursor->next;
+		return true;
+	}
+}
 
-bool isEmpty() const;
-bool isFull() const;
+bool ListNode::goToPrior( )
+{
+	if( this->isEmpty( ) )
+	{
+		return false;
+	}
+	else if( this->cursor == this->head )
+	{
+		return false;
+	}
+	else
+	{
+		Node* temp = this->head;
+		while( temp->next != this->cursor )
+			temp = temp->next;
+
+		this->cursor = temp;
+
+		return true;
+	}
+}
+
+bool ListNode::isEmpty( ) const
+{
+	if( this->head == NULL && this->cursor == NULL )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool ListNode::isFull() const;
 
 ListNode& operator = (const ListNode&);
 
